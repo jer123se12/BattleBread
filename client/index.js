1cec1e3aa58e4c794782cfc5d4b1aa864bcd333c
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2022-07-15 11:12:10 +0800

    Basic Socket.io

--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 import { io } from "socket.io-client";
 
+// Socket.io Server
 const socket = io('http://localhost:3000')
 
 socket.on("connect", () => {
     console.log(`Connected with ${socket.id}`)
+    document.getElementById("roomID").innerHTML = `Room ID: ${socket.id}`
 })
 
-document.getElementById("roomID").innerHTML = socket.id
+// HTML
+const joinRoomBtn = document.getElementById("join-btn")
+const roomIDInput = document.getElementById("id-input")
+
+joinRoomBtn.addEventListener("click", () => {
+    const roomID = roomIDInput.value
+})
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const io = require("socket.io")(3000, {
     cors: {
-        origin: ['http://172.0.0.1:5500']
+        origin: ['http://localhost:8080']
     }
 })
 io.on('connection', (socket) => {
     console.log(`User Connected with ${socket.id}`)
+
+    socket.on("send-tile", (tile, room) => {
+        if (room !== "") {
+            socket.to(room).emit("recieve-tile", tile)
+        }
+    })
 
     socket.on('disconnect', () => {
         console.log('User Disconnected')
